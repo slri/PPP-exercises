3dbe05f2813bb079272802304e3d03bc30c68c70
sIri <bljuzgavic.22@gmail.com>
2018-07-28 22:13:24 +0200

    exercise 3

---
+++ ch3/ex3/ex3.cpp
@@ -0,0 +1,37 @@
+﻿// ex3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "../../include/std_lib_facilities.h"
+
+
+int main()
+{
+	//int double = 48; //illegal
+	//int _toStrin = 9; //works
+	//string int = "memes"; //illegal
+	//my wow; //my identifier is underfined
+	//int 0ctopus = 1; //no identifier
+	//int string = 1; //overwritten
+	//double int = 1.1; //illegal
+	//int kebab-case = 1; //expected ;
+	//int _toString(); //initialized
+	//string string = "wwww"; //works but i'm guessing it overwrites
+	//int .see = 1; //expected identifier
+	//int integer = 1; //works
+	//int *hi = 1; //value of type int can't be used to init int*
+	//note: most special characters just say there's no identifier
+	//int h*i = 1; //expected ; makes sense cuz it'll prolly try to save h as the varname, settle for definition, and then poop pant cuz no line terminator
+	//int Čip = 1; //unicode works
+	//int <i> = 1; //same as the rest of special chars
+	//int #wow = 1; //not expected #
+	//int $wow = 1; //works??? works.
+	//int &wow = 1; //init ref of non-const reference must be a lvalue
+	//int ‚wow = 1; //works?? ????
+	//int ~wow = 1; //invalid destructor declaration
+	//int @wow = 1; //unrecognized token
+
+	keep_window_open();
+    return 0;
+}
+
